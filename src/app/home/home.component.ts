3a8b02a0c3a7a17da80a2936dfab6170d719d2f4
kannanATexcelforte <kannan@excelforte.com>
2024-12-01 20:15:34 +0530

    01.12.2024-cc
    completed

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingLocation } from '../housing-location';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   imports: [
+    CommonModule,    
     HousingLocationComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  housingLocationList: HousingLocation[] = [
+    {
+      name: 'Acme Fresh Start Housing',
+      city: 'Chicago'
+    },
+    {
+      name: 'A113 Transitional Housing',
+      city: 'Santa Monica'
+    }
+  ];
 }
---
+++ src/app/housing-location.ts
@@ -0,0 +1,4 @@
+export interface HousingLocation {
+    name: string,
+    city: string
+}
