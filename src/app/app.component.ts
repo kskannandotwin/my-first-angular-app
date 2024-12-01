3a9d478aa6a18a054547c9f0c1fffd8e9fa879cd
kannanATexcelforte <kannan@excelforte.com>
2024-12-01 19:26:22 +0530

    01.12.2024-cin
    completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   imports: [
-    // RouterOutlet,
+    RouterOutlet,
+    HomeComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
