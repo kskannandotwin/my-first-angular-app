f78a9105dcf3bf922ff0d0884dc0d7c89ed83760
kannanATexcelforte <kannan@excelforte.com>
2024-12-02 21:19:58 +0530

    02.12.2024-fin
    completed

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -3,11 +3,13 @@ import { Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HousingService } from '../housing.service';
 import { HousingLocation } from '../housing-location';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-details',
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule
   ],
   templateUrl: './details.component.html',
   styleUrl: './details.component.css'
@@ -17,8 +19,21 @@ export class DetailsComponent {
   housingService = inject(HousingService);
   housingLocation: HousingLocation | undefined;
 
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl('')
+  });
   constructor() {
     const housingLocationid = Number(this.route.snapshot.params['id']);
     this.housingLocation = this.housingService.getHousingLocationById(housingLocationid);
+  }
+
+  submitApplication() {
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? ''
+    )
   }
 }
--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -27,6 +27,10 @@ export class HousingService {
   getHousingLocationById(id: number): HousingLocation | undefined {
     return this.housingLocationList.find(housingLocation => housingLocation.id === id);
   }
+
+  submitApplication(firstName: string, lastName: string, email: string) {
+    console.log(`Housing application received: firstName: ${firstName}, lastName: ${lastName}, email: ${email}`);
+  }
 }
 
 
